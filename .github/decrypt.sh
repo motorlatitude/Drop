222b519ff1c237071e5e100057b800be804cc49f
Lennart Hase <1834270+motorlatitude@users.noreply.github.com>
2020-04-05 22:36:04 +0100

    :green_heart: Fixing decrypt function for linux

--- .github/decrypt.sh
+++ .github/decrypt.sh
@@ -2,4 +2,4 @@
 
 # --batch to prevent interactive command --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$ENV_SECRET" \
---output ../electron-builder.env ../electron-builder.env.gpg
+--output ./electron-builder.env ./electron-builder.env.gpg
