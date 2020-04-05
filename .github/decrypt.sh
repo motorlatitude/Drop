b2877a5c3e3cdb296bce7775042e5b71540b9338
Lennart Hase <1834270+motorlatitude@users.noreply.github.com>
2020-04-05 23:02:39 +0100

    :green_heart: fixing ci

--- .github/decrypt.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# --batch to prevent interactive command --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$ENV_SECRET" \
---output ../electron-builder.env ../electron-builder.env.gpg
